772621bf24a42d39b65288f1588dc178c6b64066
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-03-19 20:58:40 +0600

    create pdf and download from client app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./app.css";
+import CreatePdf from "./components/CreatePdf";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <CreatePdf />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/CreatePdf.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
+
+const CreatePdf = () => {
+  const [state, setState] = useState({
+    name: "",
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
+  });
+
+  const handleChange = ({ target: { value, name } }) =>
+    setState({ ...state, [name]: value });
+
+  const createAndDownloadPdf = () => {
+    axios
+      .post("/create-pdf", state)
+      .then(() => axios.get("fetch-pdf", { responseType: "blob" }))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+
+        saveAs(pdfBlob, "newPdf.pdf");
+      });
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Receipt ID"
+        name="receiptId"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Price 1"
+        name="price1"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Price 2"
+        name="price2"
+        onChange={handleChange}
+      />
+      <button onClick={createAndDownloadPdf}>Download PDF</button>
+    </div>
+  );
+};
+
+export default CreatePdf;
